756b83c769b15eddf60380768fe7cfb59bf9edb3
Hunter Stevens <onebree@gmail.com>
2017-03-05 17:00:29 -0500

    Add presenter skeletons

---
+++ app/presenters/comment_presenter.rb
@@ -0,0 +1,9 @@
+class CommentPresenter
+  def initialize(comment)
+  end
+
+  attr_reader :comment
+
+  def content
+  end
+end
---
+++ app/presenters/link_presenter.rb
@@ -0,0 +1,9 @@
+class LinkPresenter
+  def initialize(link)
+  end
+
+  attr_reader :link
+
+  def content
+  end
+end
