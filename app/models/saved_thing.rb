7dafbf3d8a18415116ebc76ae6a9a6578885912f
Hunter Stevens <onebree@gmail.com>
2017-03-13 21:54:54 -0400

    i need to log off -- too many typos

--- app/models/saved_thing.rb
+++ app/models/saved_thing.rb
@@ -1,6 +1,6 @@
 class SavedThing < ApplicationRecord
   has_many :user_saved_things
-  has_many :users. :through => :user_saved_things
+  has_many :users, :through => :user_saved_things
 
   def kind
     case type
